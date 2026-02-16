83338667c66fe36c6b03a221d4beac9577d028e3
riccardo-ottavi <ottavi.ps@libero.it>
2026-02-16 15:27:39 +0100

    inizializza le varie pagine

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,5 +1,32 @@
-export default function ListPage(){
-    return(
-        <h1>Sono la Listpage</h1>
+import { useState, useEffect } from "react"
+
+export default function ListPage() {
+
+    const [people, setPeople] = useState([])
+
+    useEffect(() => {
+        fetchList();
+    }, [])
+
+
+    async function fetchList() {
+        const response = await fetch('http://localhost:3000/people')
+        const data = await response.json()
+        setPeople(data)
+    }
+
+    return (
+        <>
+            <h1>Sono la ListPage</h1>
+
+            {people.map(person => (
+                <div key={person?.id}>
+                    <p>{person?.firstName}</p>
+                    <p>{person?.lastName}</p>
+                    <p>{person?.birthDate}</p>
+                </div>
+            ))}
+        </>
+
     )
 }
