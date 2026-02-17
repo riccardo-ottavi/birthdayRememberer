ef6ec4396eebf59a76ff582eae14631255993f70
riccardo-ottavi <ottavi.ps@libero.it>
2026-02-17 14:56:30 +0100

    inserisci modale di conferma eliminazione

---
+++ src/components/Modal.jsx
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom";
+
+export default function Modal({ title, content, show, onClose, onConfirm, confirmText = "Conferma" }){
+    
+    return show && ReactDOM.createPortal(
+        <div className="container-modal">
+            <div className="modal">
+                <h2>{title}</h2>
+                <span>{content}</span>
+                <button onClick={onClose}>Chiudi</button>
+                <button onClick={onConfirm}>{confirmText}</button>
+            </div>
+        </div>,
+        document.body
+    )
+}
--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,39 +1,52 @@
-import { useContext } from "react"
-import { GlobalContext } from "../contexts/BirthdayContext"
+import { useContext, useState } from "react";
+import { GlobalContext } from "../contexts/BirthdayContext";
+import Modal from "../components/Modal";
 
 export default function ListPage() {
 
-    const { people, setPeople } = useContext(GlobalContext)
+    const { people, setPeople } = useContext(GlobalContext);
+    const [showRemove, setShowRemove] = useState(false);
+    const [personToRemove, setPersonToRemove] = useState(null);
 
     async function handleDelete(id) {
-    try {
-        const response = await fetch(`http://localhost:3000/people/${id}`, {
-            method: "DELETE",
-        });
-        const data = await response.json();
-        console.log(data.message, data.deletedPerson);
+        try {
+            const response = await fetch(`http://localhost:3000/people/${id}`, {
+                method: "DELETE",
+            });
+            const data = await response.json();
+            console.log(data.message, data.deletedPerson);
 
-        setPeople(prev => prev.filter(p => p.id !== id));
-
-    } catch (error) {
-        console.error("Errore eliminazione:", error);
+            setPeople(prev => prev.filter(p => p.id !== id));
+        } catch (error) {
+            console.error("Errore eliminazione:", error);
+        }
     }
-}
-
-
 
     return (
         <div className="container">
             {people?.map(person => {
-                const birthDate = new Date(person?.birthDate);
+                const birthDate = new Date(person.birthDate);
                 return (
-                    <div key={person?.id}>
-                        <p>{person?.firstName} {person?.lastName} - {birthDate?.toLocaleDateString("it-IT")}</p>
-                        <button onClick={() => handleDelete(person.id)}>Elimina</button>
+                    <div key={person.id}>
+                        <p>{person.firstName} {person.lastName} - {birthDate.toLocaleDateString("it-IT")}</p>
+                        <button onClick={() => {
+                            setPersonToRemove(person);
+                            setShowRemove(true);
+                        }}>Elimina</button>
                     </div>
                 )
             })}
-        </div>
 
+            <Modal
+                title={"Cancella"}
+                content={"Se confermi, la persona verrà rimossa dall'elenco."}
+                show={showRemove}
+                onClose={() => setShowRemove(false)}
+                onConfirm={() => {
+                    if (personToRemove) handleDelete(personToRemove.id);
+                    setShowRemove(false);
+                }}
+            />
+        </div>
     )
 }
