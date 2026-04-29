445e514cc493bc2dc20aa89c3f5e3c0111fab2b5
riccardo-ottavi <ottavi.ps@libero.it>
2026-04-29 16:48:45 +0200

    fix: routing

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,7 +3,11 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
 
 export default function ProtectedRoute({ children }) {
-  const { token } = useContext(AuthContext);
+  const { token, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return null;  
+  }
 
   if (!token) {
     return <Navigate to="/login" replace />;
